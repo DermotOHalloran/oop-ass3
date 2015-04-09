bd1bc04fc852a306fb249c5bac372d152b4c5a11
Maciej Piotrowski <c13470112@mydit.ie>
2015-04-09 13:56:01 +0100

    Pause Button
    Creating and Adding pause button

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/37/e0084e22afde00141e5ce9f3617ec808
@@ -0,0 +1,15 @@
+package ie.dit.reeageshark;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Game extends Activity {
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
--- ReeageShark/src/ie/dit/reeageshark/Game.java
+++ ReeageShark/src/ie/dit/reeageshark/Game.java
@@ -2,14 +2,38 @@ package ie.dit.reeageshark;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+
 
 public class Game extends Activity {
 	
+	View pauseButton;
+	
+	RelativeLayout Rel_main_game;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.game);
+		Rel_main_game = (RelativeLayout) findViewById(R.id.main_game_rl);
+		
+		DisplayMetrics dm = new DisplayMetrics();
+		this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+		final int heightS = dm.heightPixels;
+		final int widthS = dm.widthPixels;
+		
+		LayoutInflater myInflater = (LayoutInflater) getApplicationContext().getSystemService(getApplicationContext().LAYOUT_INFLATER_SERVICE);
+		pauseButton = myInflater.inflate(R.layout.pause, null, false);
+		pauseButton.setX(widthS - 245);
+		pauseButton.setY(0);
+		Rel_main_game.addView(pauseButton);
+		
 	}
 
 }
