93b27240be4137762ba439a988d4f1b483829c7c
Maciej Piotrowski <c13470112@mydit.ie>
2015-04-09 17:35:58 +0100

    Pause Menu

--- ReeageShark/src/ie/dit/reeageshark/Game.java
+++ ReeageShark/src/ie/dit/reeageshark/Game.java
@@ -1,20 +1,60 @@
 package ie.dit.reeageshark;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 public class Game extends Activity {
 	
 	View pauseButton;
-	
+	View PauseMenu;
+
 	RelativeLayout Rel_main_game;
+	 
 	
+   
+	
+	OnClickListener Continue_list = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			PauseMenu.setVisibility(View.GONE);
+			pauseButton.setVisibility(View.VISIBLE);
+		
+		}
+	};
+	
+OnClickListener To_Main_Menu_list = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			Game.this.finish();
+			
+		}
+	};
+	
+	OnClickListener Pausa_click =new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			pauseButton.setVisibility(View.GONE);
+			PauseMenu.setVisibility(View.VISIBLE);
+			
+			//Pause Starts
+			
+		}
+	};
+			
+			
+			
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -33,7 +73,19 @@ public class Game extends Activity {
 		pauseButton.setX(widthS - 245);
 		pauseButton.setY(0);
 		Rel_main_game.addView(pauseButton);
+		pauseButton.setOnClickListener(Pausa_click);
+		pauseButton.setOnClickListener(Pausa_click);pauseButton.setOnClickListener(Pausa_click);pauseButton.setOnClickListener(Pausa_click);
+		pauseButton.getLayoutParams().height=250;
+		pauseButton.getLayoutParams().width=250;
 		
+		PauseMenu= myInflater.inflate(R.layout.pause_menu, null, false);
+		Rel_main_game.addView(PauseMenu);
+		PauseMenu.setVisibility(View.GONE);//hiding the pause menu the screen
+		
+		ImageView Cont = (ImageView)PauseMenu.findViewById(R.id.imCont);
+		ImageView MainMenuTo = (ImageView)PauseMenu.findViewById(R.id.toMain);
+		Cont.setOnClickListener(Continue_list);
+		MainMenuTo.setOnClickListener(To_Main_Menu_list);
 	}
 
 }
